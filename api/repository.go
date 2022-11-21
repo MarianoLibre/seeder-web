b229700391abee3a0c7c6bfd29b491101a324ada
MarianoLibre <mariano.macri@mercadolibre.com>
2022-11-21 14:08:41 -0300

    add queries

--- api/repository.go
+++ api/repository.go
@@ -37,6 +37,89 @@ const (
 		warehouses,
 		sections
 	`
+	INSERT_WAREHOUSES = `INSERT INTO warehouses (
+		address,
+		telephone,
+		warehouse_code,
+		minimum_capacity,
+		minimum_temperature) VALUES (?, ?, ?, ?, ?)`
+	INSERT_BUYERS = `INSERT INTO buyers (
+		card_number_id,
+		first_name,
+		last_name,
+		purchase_order_count) VALUES (?, ?, ?, ?)`
+	INSERT_SELLERS = `INSERT INTO sellers (
+		cid,
+		company_name,
+		address,
+		telephone,
+		locality_id) VALUES (?, ?, ?, ?, ?)`
+	INSERT_SECTIONS = `INSERT INTO sections (
+		section_number,
+		current_temperature,
+		minimum_temperature,
+		current_capacity,
+		minimum_capacity,
+		maximum_capacity,
+		warehouse_id,
+		id_product_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+	INSERT_EMPLOYEES = `INSERT INTO employees (
+		card_number_id,
+		first_name,
+		last_name,
+		warehouse_id) VALUES (?, ?, ?, ?)`
+	INSERT_PRODUCTS = `INSERT INTO products (
+		description,
+		expiration_date,
+		freezing_rate,
+		height,
+		lenght,
+		netweight,
+		product_code,
+		recommended_freezing_temperature,
+		width,
+		id_product_type,
+		id_seller) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	INSERT_LOCALITIES = `INSERT INTO localities (
+		locality_name,
+		province_name,
+		country_name,
+		zip_code) VALUES (?, ?, ?, ?)`
+	INSERT_CARRIES = `INSERT INTO carries (
+		cid,
+		company_name,
+		address,
+		telephone,
+		locality_id) VALUES (?, ?, ?, ?, ?)`
+	INSERT_INBOUND_ORDERS = `INSERT INTO inbound_orders (
+		order_date,
+		order_number,
+		employee_id,
+		product_batch_id,
+		warehouse_id) VALUES (?, ?, ?, ?, ?`
+	INSERT_PRODUCT_BATCHES = `INSERT INTO product_batches (
+		batch_number,
+		current_quantity,
+		current_temperature,
+		due_date,
+		initial_quantity,
+		manufacturing_date,
+		manufacturing_hour,
+		minimum_temperature,
+		product_id,
+		section_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+	INSERT_PRODUCT_RECORDS = `INSERT INTO product_records (
+		last_updated_date,
+		purchase_price,
+		sale_price,
+		product_id) VALUES (?, ?, ?, ?)`
+	INSERT_PURCHASE_ORDERS = `INSERT INTO purchase_orders (
+		order_number,
+		order_date,
+		tracking_code,
+		buyer_id,
+		product_record_id,
+		order_status_id) VALUES (?, ?, ?, ?, ?, ?)`
 )
 
 
@@ -67,3 +150,65 @@ func (r *repository) CreateTables() error {
 	}
 	return nil
 }
+
+// Seeders
+func SeedWarehouses(n int) error {
+
+	return nil
+}
+
+func SeedBuyers(n int) error {
+
+	return nil
+}
+
+func SeedSellers(n int) error {
+
+	return nil
+}
+
+func SeedEmployees(n int) error {
+
+	return nil
+}
+
+func SeedProducts(n int) error {
+
+	return nil
+}
+
+func SeedSections(n int) error {
+
+	return nil
+}
+
+func SeedLocalities(n int) error {
+
+	return nil
+}
+
+func SeedCarries(n int) error {
+
+	return nil
+}
+
+func SeedProductBatches(n int) error {
+
+	return nil
+}
+
+func SeedProductOrders(n int) error {
+
+	return nil
+}
+
+func SeedInboundOrders(n int) error {
+
+	return nil
+}
+
+func SeedPurchaseOrders(n int) error {
+
+	return nil
+}
+
