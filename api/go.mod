d9d850711df814fb208b6563127b8c4fd5c8f8ef
MarianoLibre <mariano.macri@mercadolibre.com>
2022-11-21 13:21:02 -0300

    load files from "dist", even when names will change

--- api/go.mod
+++ api/go.mod
@@ -3,8 +3,8 @@ module example.com
 go 1.19
 
 require (
+	github.com/fatih/color v1.13.0
 	github.com/gin-contrib/cors v1.4.0
-	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.4.0
@@ -18,6 +18,7 @@ require (
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- api/routes.go
+++ api/routes.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
+	"github.com/fatih/color"
 )
 
 type router struct {
@@ -14,18 +18,38 @@ type router struct {
 func NewRouter(eng *gin.Engine, db *sql.DB) router {
 	r := router{engine: eng, database: db}
 
-	r.engine.LoadHTMLGlob("index.html")
-
+	// Serve the front
 	r.engine.Static("/foo", "../web/dist")
-	r.engine.StaticFile("/index.b18976e1.css", "../web/dist/index.b18976e1.css")
-	r.engine.StaticFile("/index.b38dac7d.js", "../web/dist/index.b38dac7d.js")
 
+	files, err := ioutil.ReadDir("../web/dist")
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range files {
+		fname := f.Name()
+		re := regexp.MustCompile(`.*\.css$|.*\.js$`)
+		if re.MatchString(fname) {
+			r.engine.StaticFile(
+				fmt.Sprintf("/%s", fname),
+				fmt.Sprintf("../web/dist/%s", fname),
+			)
+			msg := fmt.Sprintf("[ File: '%s' loaded ]\n", f.Name())
+			color.New(color.BgBlue).Add(color.FgBlack).Print(msg)
+		}
+	}
+
+	// Connect to the db
 	repo := NewRepository(db)
 	handler := NewSeeder(repo)
 
+	// Dummy "it's running"
+	r.engine.LoadHTMLGlob("index.html")
 	r.engine.GET("", handler.SayHi())
+
+	// Routes
 	group := r.engine.Group("/seeder")
 	group.GET("/drop-tables", handler.Drop())
 	group.GET("/create-tables", handler.Create())
+
 	return r
 }
